eb63a657f551bf1779685e6acad53f606b528375
Andrew Le <a1le@ucsd.edu>
2020-10-20 18:07:02 -0700

    Added 12.4

---
+++ epi_judge_python/chapter_12_hashing/problem_12_4_lowest_common_ancestor_close_ancestor.py
@@ -0,0 +1,27 @@
+
+def lca(node0, node1):
+    iter0, iter1 = node0, node1
+    nodes_on_path_to_root = set()
+    while iter0 or iter1:
+        if iter0:
+            if iter0 in nodes_on_path_to_root:
+                return iter0
+            nodes_on_path_to_root.add(iter0)
+            iter0 = iter0.parent
+
+        if iter1:
+            if iter1 in nodes_on_path_to_root:
+                return iter1
+            nodes_on_path_to_root.add(iter1)
+            iter1 = iter1.parent
+
+    raise ValueError("node0 and node1 are not in the same binary tree")
+
+
+def main():
+   pass
+
+
+
+if __name__ == "__main__":
+    main()
